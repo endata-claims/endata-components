b66c30e54332109d403155c9f2473a79dad74fd8
dohaicuong <beatyshot@gmail.com>
2020-02-17 12:49:19 +1000

    fix rollup configs

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import sass from 'rollup-plugin-sass'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import resolve from 'rollup-plugin-node-resolve'
+import localResolve from 'rollup-plugin-local-resolve'
+import json from '@rollup/plugin-json'
 
 import pkg from './package.json';
 
@@ -24,16 +26,13 @@ export default {
   ],
   plugins: [
     external(),
-    resolve({
-      browser: true
-    }),
     typescript({
       rollupCommonJSResolveHack: true,
       exclude: '**/__tests__/**',
       clean: true
     }),
     commonjs({
-      include: ['node_modules/**'],
+      include: /node_modules/,
       exclude: ['**/*.stories.js'],
       namedExports: {
         'node_modules/react/react.js': [
@@ -46,12 +45,35 @@ export default {
         'node_modules/react-is/index.js': [
           'isElement',
           'isValidElementType',
-          'ForwardRef'
-        ]
+          'ForwardRef',
+          'isFragment',
+        ],
+        'node_modules/prop-types/index.js': [
+          'array',
+          'bool',
+          'func',
+          'number',
+          'object',
+          'string',
+          'symbol',
+          'any',
+          'arrayOf',
+          'element',
+          'elementType',
+          'instanceOf',
+          'node',
+          'objectOf',
+          'oneOf',
+          'oneOfType',
+          'shape',
+          'exact',
+        ],
+        'node_modules/scheduler/index.js': ['unstable_runWithPriority', 'LowPriority'],
       }
     }),
-    sass({
-      insert: true
-    })
+    sass({ insert: true }),
+    json({ include: 'node_modules/**' }),
+    localResolve(),
+    resolve({ dedupe: ['react', 'react-dom'], browser: true }),
   ]
 }
